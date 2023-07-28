d1b4d8ab59da818b63409fbe790d4e5bda96a499
Gabriela Butnaru <gabrielabutnaru24@gmail.com>
2023-07-28 14:24:41 +0300

    update the app

--- mobile/App.js
+++ mobile/App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { configTheme } from './theme';
+import { Text, View } from "react-native-ui-lib";
+
+configTheme();
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View flex center>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
